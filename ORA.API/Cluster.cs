09f058e2aad78c40c00169909ab3926ebb53c053
Adamaq01 <adamthibert01@gmail.com>
2020-02-03 20:51:48 +0100

    :construction: Added basic API classes and methods

---
+++ ORA.API/Cluster.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace ORA.API
+{
+    public abstract class Cluster
+    {
+        private string _name;
+        private string _identifier;
+
+        public string Name => this._name;
+        public string Identifier => this._identifier;
+
+        protected Cluster(string name, string identifier)
+        {
+            this._name = name;
+            this._identifier = identifier;
+        }
+
+        public abstract List<T> GetNodes<T>() where T : Node;
+
+        public abstract T GetNode<T>(string identifier) where T : Node;
+
+        public abstract void AddNode<T>(T node) where T : Node;
+
+        public abstract void RemoveNode(string identifier);
+    }
+}
---
+++ ORA.API/Managers/INodeManager.cs
@@ -0,0 +1,11 @@
+﻿namespace ORA.API.Managers
+{
+    public interface INodeManager
+    {
+        Node CreateNode(string name);
+
+        Node GetNode(string identifier);
+
+        bool DeleteNode(string identifier);
+    }
+}
