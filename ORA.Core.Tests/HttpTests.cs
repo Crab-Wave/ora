4ab8e82ed0aba668c743b6c13f097d162b2e93c0
Adamaq01 <adamthibert01@gmail.com>
2020-02-28 19:45:49 +0100

    :recycle: Rewrite tests

---
+++ ORA.Core.Tests/HttpTests.cs
@@ -0,0 +1,17 @@
+﻿using ORA.API;
+using ORA.API.Http;
+using Xunit;
+
+namespace ORA.Core.Tests
+{
+    public class Tests : IClassFixture<CoreInitializationFixture>
+    {
+        [Fact]
+        public void HttpClientTests()
+        {
+            HttpResponse httpResponse = Ora.CreateHttpClient().Get("http://httpbin.org/get");
+            Assert.NotNull(httpResponse);
+            Assert.NotEmpty(httpResponse.Body);
+        }
+    }
+}
