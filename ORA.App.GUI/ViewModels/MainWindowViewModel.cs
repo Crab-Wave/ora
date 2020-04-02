454b530e3fb7f8d9045295cdbdcee00af2c1527f
TrAyZeN <1810leo@gmail.com>
2020-04-02 19:52:11 +0200

    :construction: Change namespace due to conflict with Avalonia and init app with Avalonia template

---
+++ ORA.App.GUI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ORA.App.GUI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
---
+++ ORA.App.GUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace ORA.App.GUI.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
