4989cd84f9b5e9eb7ac434194014b4cf9ccfafaf
Raffaël MORAISIN <raffael.moraisin@epita.fr>
2020-04-20 17:37:12 +0200

    :lipstick: :fire: :sparkles: modification of the get method + delete of the model and the validator if the public key + creation of the generate method

--- ORA.Application.CLI/Identities.cs
+++ ORA.Application.CLI/Identities.cs
@@ -9,12 +9,19 @@ namespace ORA.Application.CLI
     [Command(Description = "Identities management command", Name = "identities")]
     public class Identities
     {
-        [Command(Description = "Get the pubilc key ", Name = "get", Usage = "get")]
-
-        public void Get([Operand(Description = "get identity")] IdentityPublicKeyModel publicKey)
+        [Command(Description = "Get the public key ", Name = "get", Usage = "get")]
+        public void Get()
         {
-            publicKey.PublicKey = Ora.GetIdentityManager().GetIdentity().PublicKey;
-            Console.WriteLine($"Your public key is {publicKey.PublicKey}");
+            byte[] publicKey = Ora.GetIdentityManager().GetIdentity().PublicKey;
+            Console.WriteLine($"Your public key is {publicKey}");
+        }
+
+        [Command(Description = "Generate a new identity ", Name = "generate", Usage = "generate")]
+        public void Generate()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            Ora.GetIdentityManager().GenerateIdentity(path);
+            Console.WriteLine("An identity has been generate");
         }
     }
 }
