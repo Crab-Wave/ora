0be0933ebd3cc8da796859b32e5ea8cdae57b6e2
Raffaël MORAISIN <raffael.moraisin@epita.fr>
2020-04-21 23:02:09 +0200

    :fire: delete IMemberManager.cs

--- ORA.API/Managers/IMemberManager.cs
+++
@@ -1,9 +0,0 @@
-namespace ORA.API.Managers
-{
-    public interface IMemberManager
-    {
-        Member[] GetMembers();
-        Member GetMember(string identifier);
-        bool RemoveMember(string identifier);
-    }
-}
