907d1a61d77ded9f5d4e9606ebb0f25147481b33
TrAyZeN <1810leo@gmail.com>
2020-04-23 17:07:29 +0200

    :construction: Add content to settings vue and navigation

--- ORA.App.GUI/ViewModels/SettingsViewModel.cs
+++ ORA.App.GUI/ViewModels/SettingsViewModel.cs
@@ -1,12 +1,26 @@
 using ReactiveUI;
+using ORA.API;
 
 namespace ORA.App.GUI.ViewModels
 {
     public class SettingsViewModel : ViewModelBase
     {
+        private string baseUrl;
+        public string BaseUrl
+        {
+            get => baseUrl;
+            set => this.RaiseAndSetIfChanged(ref baseUrl, value);
+        }
+
         public SettingsViewModel()
         {
+            this.baseUrl = Ora.GetHttpClient().GetBaseUrl();
+        }
 
+        public void SetBaseUrl()
+        {
+            Ora.GetHttpClient().SetBaseUrl(this.BaseUrl);
+            Ora.GetAuthManager().Authenticate();
         }
     }
 }
